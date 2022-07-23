c5ba2f0ef3d424b39725d6c3782994c9f900490a
Dimex19 <72265654+Dimex19@users.noreply.github.com>
2022-07-23 18:48:32 +0100

    Update recommendation.py

--- recommendation.py
+++ recommendation.py
@@ -4,8 +4,8 @@ import re
 import pandas as pd
 import numpy as np
 import re
-from app import app
-server = app.server
+import app
+app = app.run
 
 #creating a function that cleans the title
 def clean_title(title):
