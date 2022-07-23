f708f892c6437a8b51fbb5e9ba01d814845a1ade
Dimex19 <72265654+Dimex19@users.noreply.github.com>
2022-07-23 02:13:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ CORS(app)
         
 @app.route('/movie', methods=['GET'])
 def recommend_movies():
-    res = recommendation.results(request.args.get('title'))
+    res = recommendation.find_similar_movies(request.args.get('title'))
     return jsonify(res)
 
 if __name__=='__main__':
